a0cacb0312687eff0a60890708a4466d6e81ee7d
Adrien Bounader <adrien.bounade@wiilog.fr>
2019-10-28 11:04:13 +0100

    WIIS-1143: correction  * préférer les initialisations dans le constructeur  * Nettoyage & optimisation  * réglage pour bug lorsque le scroll arrive à la fin

--- src/pages/connect/connect.ts
+++ src/pages/connect/connect.ts
@@ -20,7 +20,7 @@ export class ConnectPage {
         password: ''
     };
     public connectURL: string = '/api/connect';
-    public isLoaded: boolean = false;
+    public isLoaded: boolean;
 
     public constructor(public navCtrl: NavController,
                        public navParams: NavParams,
--- src/pages/traca/depose-emplacement/depose-emplacement-traca.ts
+++ src/pages/traca/depose-emplacement/depose-emplacement-traca.ts
@@ -1,13 +1,15 @@
 import {Component, ViewChild} from '@angular/core';
-import {App, IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
-import {MenuPage} from "../../menu/menu";
-import {Emplacement} from "../../../app/entities/emplacement";
-import {Article} from "../../../app/entities/article";
-import {SqliteProvider} from "../../../providers/sqlite/sqlite";
+import {App, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {MenuPage} from '@pages/menu/menu';
+import {Emplacement} from '@app/entities/emplacement';
+import {Article} from '@app/entities/article';
+import {SqliteProvider} from '@providers/sqlite/sqlite';
 import {ChangeDetectorRef} from '@angular/core';
 import {IonicSelectableComponent} from 'ionic-selectable';
-import {DeposeArticlesPageTraca} from "../depose-articles/depose-articles-traca";
-import {BarcodeScannerManagerService} from "../../../app/services/barcode-scanner-manager.service";
+import {DeposeArticlesPageTraca} from '@pages/traca/depose-articles/depose-articles-traca';
+import {BarcodeScannerManagerService} from '@app/services/barcode-scanner-manager.service';
+import {ToastService} from '@app/services/toast.service';
+
 
 @IonicPage()
 @Component({
@@ -15,32 +17,29 @@ import {BarcodeScannerManagerService} from "../../../app/services/barcode-scanne
     templateUrl: 'depose-emplacement-traca.html',
 })
 export class DeposeEmplacementPageTraca {
+
+    private static readonly INIT_END_INDEX: number = 20;
+
+
     @ViewChild('locationComponent') locationComponent: IonicSelectableComponent;
     emplacement: Emplacement;
     // locationLabel = '';
     db_locations: Array<Emplacement>;
     db_locations_for_list: Array<Emplacement>;
     db_articles: Array<Article>;
-    public canGoBackService = true;
-    endIndex : number = 20;
+
+    private endIndex: number;
 
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 public app: App,
                 public sqliteProvider: SqliteProvider,
-                public barcodeScannerManager: BarcodeScannerManagerService,
+                private barcodeScannerManager: BarcodeScannerManagerService,
                 private changeDetectorRef: ChangeDetectorRef,
-                public toastController: ToastController) {
-        if (navParams.get('selectedEmplacement') !== undefined) {
-            this.emplacement = navParams.get('selectedEmplacement');
-        }
-        this.sqliteProvider.findAll('emplacement').subscribe((value) => {
-            this.db_locations = value;
-            this.db_locations_for_list = value;
-            this.sqliteProvider.findAll('article').subscribe((value) => {
-                this.db_articles = value;
-            })
-        });
+                private toastService: ToastService) {
+
+        this.resetEndIndex();
+
         let instance = this;
         (<any>window).plugins.intentShim.registerBroadcastReceiver({
                 filterActions: [
@@ -55,6 +54,20 @@ export class DeposeEmplacementPageTraca {
             });
     }
 
+    public ionViewWillEnter(): void {
+        if (this.navParams.get('selectedEmplacement') !== undefined) {
+            this.emplacement = this.navParams.get('selectedEmplacement');
+        }
+        this.sqliteProvider.findAll('emplacement').subscribe((value) => {
+            this.db_locations = value;
+            this.db_locations_for_list = value;
+        });
+    }
+
+    public ionViewCanLeave(): boolean {
+        return this.barcodeScannerManager.canGoBack;
+    }
+
     goToArticles() {
         this.navCtrl.push(DeposeArticlesPageTraca, {emplacement: this.emplacement});
     }
@@ -63,21 +76,15 @@ export class DeposeEmplacementPageTraca {
         this.emplacement = event.value;
     }
 
-    searchEmplacement(event: { component: IonicSelectableComponent, text: string }) {
+    searchEmplacement({text}: { component: IonicSelectableComponent, text: string }) {
         this.locationComponent.showLoading();
-        if (event.text.trim() === '') {
-            this.db_locations_for_list = this.db_locations.slice(0, this.endIndex);
-            this.changeDetectorRef.detectChanges();
-        } else if (event.text.trim().length > 2) {
-            let text = event.text.trim();
-            this.db_locations_for_list = this.db_locations.filter(emplacement => emplacement.label.toLowerCase().includes(text.toLowerCase())).slice(0, this.endIndex);
-            this.changeDetectorRef.detectChanges();
-        }
-        this.locationComponent.hideLoading();
-    }
+        this.changeDetectorRef.detectChanges();
 
-    ionViewCanLeave() {
-        return this.barcodeScannerManager.canGoBack;
+        this.resetEndIndex();
+        this.updateLocationForList(text, true);
+
+        this.locationComponent.hideLoading();
+        this.changeDetectorRef.detectChanges();
     }
 
     goHome() {
@@ -95,40 +102,47 @@ export class DeposeEmplacementPageTraca {
                 this.navCtrl.push(DeposeEmplacementPageTraca, {selectedEmplacement: emplacement});
                 this.changeDetectorRef.detectChanges();
             } else {
-                this.showToast('Veuillez flasher ou sélectionner un emplacement connu.');
+                this.toastService.showToast('Veuillez flasher ou sélectionner un emplacement connu.');
             }
         });
     }
 
-    async showToast(msg) {
-        const toast = await this.toastController.create({
-            message: msg,
-            duration: 2000,
-            position: 'center',
-            cssClass: 'toast-error'
-        });
-        toast.present();
+    getMoreLocations({text, component}: { component: IonicSelectableComponent, text: string }) {
+        this.locationComponent.showLoading();
+        if (this.endIndex >= this.db_locations.length) {
+            component.disableInfiniteScroll();
+        }
+        else {
+
+            if (this.endIndex + DeposeEmplacementPageTraca.INIT_END_INDEX >= this.db_locations.length) {
+                this.endIndex = this.db_locations.length;
+            }
+            else {
+                this.endIndex += DeposeEmplacementPageTraca.INIT_END_INDEX;
+            }
+
+            this.updateLocationForList(text);
+        }
+        component.endInfiniteScroll();
+        this.locationComponent.hideLoading();
     }
 
-    getMoreLocations(event: { component: IonicSelectableComponent, text: string }) {
-        this.locationComponent.showLoading();
-        let text = event.text.trim();
-        if (this.endIndex >= this.db_locations.length) {
-            event.component.disableInfiniteScroll();
-            return;
+    private updateLocationForList(text?: string, isSearch: boolean = false): void {
+        if (text) {
+            const trimmedText = text.trim();
+            if (!isSearch || text.length > 2) {
+                this.db_locations_for_list = this.db_locations
+                    .filter(emplacement => emplacement.label.toLowerCase().includes(trimmedText.toLowerCase()))
+                    .slice(0, this.endIndex);
+            }
         }
-        if (this.endIndex + 20 >= this.db_locations.length) {
-            this.endIndex = this.db_locations.length;
-        } else {
-            this.endIndex+=20;
-        }
-        if (text !== '') {
-            this.db_locations_for_list = this.db_locations.filter(emplacement => emplacement.label.toLowerCase().includes(text.toLowerCase())).slice(0, this.endIndex);
-        } else {
+        else {
             this.db_locations_for_list = this.db_locations.slice(0, this.endIndex);
         }
-        event.component.endInfiniteScroll();
-        this.locationComponent.hideLoading();
+    }
+
+    private resetEndIndex(): void {
+        this.endIndex = DeposeEmplacementPageTraca.INIT_END_INDEX;
     }
 
 }
