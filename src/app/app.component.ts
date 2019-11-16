094c3fa423e7c0567612c41f696f87858a986680
Adrien Bounader <adrien.bounade@wiilog.fr>
2019-11-16 19:09:19 +0100

    Modification style de la statusBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ export class AppComponent {
         this.platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
-            this.statusBar.styleDefault();
+            this.statusBar.styleBlackOpaque();
             this.splashScreen.hide();
             this.networkProvider.initializeNetworkEvents();
         });
