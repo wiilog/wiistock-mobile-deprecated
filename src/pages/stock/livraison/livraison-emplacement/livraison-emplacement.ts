f467f217b717c921da255e931bd119613c435440
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-04-18 12:42:55 +0200

    WIIS-1121 : On adapte livraison-emplacement avec les composants générique

--- src/pages/stock/livraison/livraison-emplacement/livraison-emplacement.ts
+++ src/pages/stock/livraison/livraison-emplacement/livraison-emplacement.ts
@@ -1,17 +1,18 @@
 import {Component, ViewChild} from '@angular/core';
-import {IonicPage, Navbar, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Emplacement} from '@app/entities/emplacement';
 import {SqliteProvider} from '@providers/sqlite/sqlite';
 import {Livraison} from '@app/entities/livraison';
 import {ToastService} from '@app/services/toast.service';
-import {BarcodeScannerManagerService} from '@app/services/barcode-scanner-manager.service';
-import {Observable, Subscription} from 'rxjs';
-import {SearchLocationComponent} from '@helpers/components/search-location/search-location.component';
+import {Observable} from 'rxjs';
 import {flatMap} from 'rxjs/operators';
 import 'rxjs/add/observable/zip';
 import {of} from 'rxjs/observable/of';
 import {Network} from '@ionic-native/network';
 import {LocalDataManagerService} from '@app/services/local-data-manager.service';
+import {SelectLocationComponent} from '@helpers/components/select-location/select-location.component';
+import {IconConfig} from '@helpers/components/panel/model/icon-config';
+import {BarcodeScannerModeEnum} from '@helpers/components/barcode-scanner/barcode-scanner-mode.enum';
 
 
 @IonicPage()
@@ -20,24 +21,29 @@ import {LocalDataManagerService} from '@app/services/local-data-manager.service'
     templateUrl: 'livraison-emplacement.html',
 })
 export class LivraisonEmplacementPage {
-    @ViewChild(Navbar)
-    public navBar: Navbar;
+    @ViewChild('selectLocationComponent')
+    public selectLocationComponent: SelectLocationComponent;
 
-    @ViewChild('searchComponent')
-    public searchComponent: SearchLocationComponent;
-
-    public emplacement: Emplacement;
+    public location: Emplacement;
     public livraison: Livraison;
+
+    public barcodeScannerSearchMode: BarcodeScannerModeEnum = BarcodeScannerModeEnum.TOOL_SEARCH;
+
+    public panelHeaderConfig: {
+        title: string;
+        subtitle?: string;
+        leftIcon: IconConfig;
+        rightIcon: IconConfig;
+        transparent: boolean;
+    };
 
     private validateIsLoading: boolean;
     private validateLivraison: () => void;
-    private zebraScannerSubscription: Subscription;
 
     public constructor(private navCtrl: NavController,
                        private navParams: NavParams,
                        private sqliteProvider: SqliteProvider,
                        private toastService: ToastService,
-                       private barcodeScannerManager: BarcodeScannerManagerService,
                        private network: Network,
                        private localDataManager: LocalDataManagerService) {
         this.validateIsLoading = false;
@@ -47,86 +53,75 @@ export class LivraisonEmplacementPage {
         this.validateLivraison = this.navParams.get('validateLivraison');
         this.livraison = this.navParams.get('livraison');
 
-        this.zebraScannerSubscription = this.barcodeScannerManager.zebraScan$.subscribe((barcode) => {
-            this.testLocation(barcode);
-        });
+        this.panelHeaderConfig = this.createPanelHeaderConfig();
+
+        if (this.selectLocationComponent) {
+            this.selectLocationComponent.fireZebraScan();
+        }
     }
 
     public ionViewWillLeave(): void {
-        if (this.zebraScannerSubscription) {
-            this.zebraScannerSubscription.unsubscribe();
-            this.zebraScannerSubscription = undefined;
+        if (this.selectLocationComponent) {
+            this.selectLocationComponent.unsubscribeZebraScan();
         }
     }
 
     public ionViewCanLeave(): boolean {
-        return this.barcodeScannerManager.canGoBack;
+        return !this.selectLocationComponent || !this.selectLocationComponent.isScanning;
     }
 
-    public scan(): void {
-        this.barcodeScannerManager.scan().subscribe((barcode) => {
-            this.testLocation(barcode);
-        });
-    }
 
-    public testLocation(locationToTest: string): void {
-        const location = this.searchComponent.isKnownLocation(locationToTest);
-        if (location) {
-            if (this.livraison.emplacement === locationToTest) {
-                this.emplacement = location;
-            }
-            else {
-                this.toastService.presentToast("Vous n'avez pas scanné le bon emplacement (destination demandée : " + this.livraison.emplacement + ")")
-            }
+    public selectLocation(locationToTest: Emplacement): void {
+        if (this.livraison.emplacement === locationToTest.label) {
+            this.location = locationToTest;
+            this.panelHeaderConfig = this.createPanelHeaderConfig();
         }
         else {
-            this.toastService.presentToast('Veuillez scanner ou sélectionner un emplacement connu.');
+            this.toastService.presentToast("Vous n'avez pas scanné le bon emplacement (destination demandée : " + this.livraison.emplacement + ")")
         }
     }
 
     public validate(): void {
         if (!this.validateIsLoading) {
-            if (this.emplacement && this.emplacement.label !== '') {
-                if (this.livraison.emplacement !== this.emplacement.label) {
-                    this.toastService.presentToast("Vous n'avez pas scanné le bon emplacement (destination demandée : " + this.livraison.emplacement + ")");
-                }
-                else {
-                    this.validateIsLoading = true;
-                    this.sqliteProvider
-                        .findArticlesByLivraison(this.livraison.id)
-                        .pipe(
-                            flatMap((articles) => Observable.zip(
-                                ...articles.map((article) => (
-                                    this.sqliteProvider
-                                        .findMvtByArticleLivraison(article.id)
-                                        .pipe(flatMap((mvt) => this.sqliteProvider.finishMvt(mvt.id, this.emplacement.label)))
-                                ))
-                            )),
-                            flatMap(() => this.sqliteProvider.finishLivraison(this.livraison.id, this.emplacement.label)),
-                            flatMap((): any => (
-                                (this.network.type !== 'none')
-                                    ? this.localDataManager.sendFinishedProcess('livraison')
-                                    : of({offline: true})
+            if (this.location && this.location.label) {
+                this.validateIsLoading = true;
+                this.sqliteProvider
+                    .findArticlesByLivraison(this.livraison.id)
+                    .pipe(
+                        flatMap((articles) => Observable.zip(
+                            ...articles.map((article) => (
+                                this.sqliteProvider
+                                    .findMvtByArticleLivraison(article.id)
+                                    .pipe(flatMap((mvt) => this.sqliteProvider.finishMvt(mvt.id, this.location.label)))
                             ))
-                        )
-                        .subscribe(
-                            ({offline, success}: any) => {
-                                if (offline) {
-                                    this.toastService.presentToast('Livraison sauvegardée localement, nous l\'enverrons au serveur une fois internet retrouvé');
-                                    this.closeScreen();
-                                }
-                                else {
-                                    this.handleLivraisonSuccess(success.length);
-                                }
-                            },
-                            (error) => {
-                                this.handleLivraisonError(error);
-                            });
-                }
+                        )),
+                        flatMap(() => this.sqliteProvider.finishLivraison(this.livraison.id, this.location.label)),
+                        flatMap((): any => (
+                            (this.network.type !== 'none')
+                                ? this.localDataManager.sendFinishedProcess('livraison')
+                                : of({offline: true})
+                        ))
+                    )
+                    .subscribe(
+                        ({offline, success}: any) => {
+                            if (offline) {
+                                this.toastService.presentToast('Livraison sauvegardée localement, nous l\'enverrons au serveur une fois internet retrouvé');
+                                this.closeScreen();
+                            }
+                            else {
+                                this.handleLivraisonSuccess(success.length);
+                            }
+                        },
+                        (error) => {
+                            this.handleLivraisonError(error);
+                        });
             }
             else {
                 this.toastService.presentToast('Veuillez sélectionner ou scanner un emplacement.');
             }
+        }
+        else {
+            this.toastService.presentToast('Chargement en cours...');
         }
     }
 
@@ -154,5 +149,21 @@ export class LivraisonEmplacementPage {
         this.navCtrl.pop().then(() => {
             this.validateLivraison();
         });
+    }
+
+    private createPanelHeaderConfig(): { title: string; subtitle?: string; leftIcon: IconConfig; rightIcon: IconConfig; transparent: boolean;} {
+        return {
+            title: 'Emplacement sélectionné',
+            subtitle: this.location && this.location.label,
+            transparent: true,
+            leftIcon: {
+                name: 'delivery.svg'
+            },
+            rightIcon: {
+                name: 'check.svg',
+                color: 'success',
+                action: () => this.validate()
+            }
+        };
     }
 }
