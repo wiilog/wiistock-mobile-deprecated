4b2dc81af1bd769b4c7f0510aadf6e29bffda9ad
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-06-05 19:22:42 +0200

    WIIS-1128 : fix prod compilation error

--- src/app/common/components/panel/model/form-panel-item-available.ts
+++ src/app/common/components/panel/model/form-panel-item-available.ts
@@ -1,4 +1,4 @@
 import {FormPanelInputConfig} from '@app/common/components/panel/model/form-panel/form-panel-input-config';
 import {FormPanelSigningConfig} from '@app/common/components/panel/model/form-panel/form-panel-signing-config';
 
-export type FormPanelItemAvailable = FormPanelInputConfig|FormPanelSigningConfig;
+export type FormPanelItemAvailable = FormPanelInputConfig & FormPanelSigningConfig;
