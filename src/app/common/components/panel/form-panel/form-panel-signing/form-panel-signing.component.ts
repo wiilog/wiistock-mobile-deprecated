ec36c30c7d32f238c099b1f8a39df9969a9a49ba
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-06-23 15:50:40 +0200

    WIIS-2456 : signature OK - suppression de la librairie de signature + implémentation manuelle de la signature

--- src/app/common/components/panel/form-panel/form-panel-signing/form-panel-signing.component.ts
+++ src/app/common/components/panel/form-panel/form-panel-signing/form-panel-signing.component.ts
@@ -1,8 +1,8 @@
 import {Component, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
 import {LoadingService} from '@app/common/services/loading.service';
-import {map} from 'rxjs/operators';
+import {delay, flatMap} from 'rxjs/operators';
 import {ModalController} from '@ionic/angular';
-import {from} from 'rxjs';
+import {from, of, Subscription, zip} from 'rxjs';
 import {SignaturePadComponent} from '@app/common/components/signature-pad/signature-pad.component';
 import {FormPanelSigningConfig} from '@app/common/components/panel/model/form-panel/form-panel-signing-config';
 
@@ -34,12 +34,11 @@ export class FormPanelSigningComponent implements OnDestroy {
     @Output()
     public valueChange: EventEmitter<string>;
 
-    private destroyed: boolean;
+    private itemClickedSubscription: Subscription;
 
     public constructor(private modalController: ModalController,
                        private loadingService: LoadingService) {
         this.valueChange = new EventEmitter<string>();
-        this.destroyed = false;
     }
 
     public get error(): string {
@@ -49,40 +48,52 @@ export class FormPanelSigningComponent implements OnDestroy {
     }
 
     public onItemClicked(): void {
-        // we wait for the keyboard to finish hiding
-        setTimeout(() =>  {
-            this.loadingService
+        if (!this.itemClickedSubscription) {
+            this.itemClickedSubscription = this.loadingService
                 .presentLoading()
                 .pipe(
-                    map((loading) => from(loading.dismiss()))
-                )
-                .subscribe(() => {
-                    if (!this.destroyed) {
+                    flatMap((loading: HTMLIonLoadingElement) => zip(
+                        of(loading),
                         from(this.modalController.create({
                             component: SignaturePadComponent,
                             componentProps: {
                                 signature: this.value
                             },
                             showBackdrop: true
-                        })).subscribe((modal: HTMLIonModalElement) => {
-                            modal.onDidDismiss().then((returnedData) => {
-                                const data = returnedData && returnedData.data;
-                                if (data && (data.signature === false || data.signature)) {
-                                    this.value = data.signature
-                                        ? data.signature // not false
-                                        : undefined; // if false we delete previous image
-                                    this.valueChange.emit(this.value);
-                                }
-                            });
-                            modal.present();
+                        }))
+                    )),
+                    delay(400), // we wait for the keyboard to finish hiding
+                    flatMap(([loading, modal]: [HTMLIonLoadingElement, HTMLIonModalElement]) => {
+                        modal.onDidDismiss().then((returnedData) => {
+                            const data = returnedData && returnedData.data;
+                            if (data && (data.signature === false || data.signature)) {
+                                this.value = data.signature
+                                    ? data.signature // not false
+                                    : undefined; // if false we delete previous image
+                                this.valueChange.emit(this.value);
+                            }
                         });
-
-                    }
+                        return zip(
+                            of(loading),
+                            from(modal.present())
+                        );
+                    })
+                )
+                .subscribe(([loading]: [HTMLIonLoadingElement, void]) => {
+                    loading.dismiss();
+                    this.unsubscribeClick();
                 });
-        }, 400);
+        }
     }
 
     public ngOnDestroy(): void {
-        this.destroyed = true;
+        this.unsubscribeClick();
+    }
+
+    private unsubscribeClick(): void {
+        if (this.itemClickedSubscription) {
+            this.itemClickedSubscription.unsubscribe();
+            this.itemClickedSubscription = undefined;
+        }
     }
 }
