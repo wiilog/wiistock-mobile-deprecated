9bb053e47ffa9cfd91b44a41c6ddd16001744687
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-11-03 21:16:38 +0100

    fix compilation

--- src/app/common/components/panel/model/form-panel/configs/form-panel-select-config.ts
+++ src/app/common/components/panel/model/form-panel/configs/form-panel-select-config.ts
@@ -1,10 +1,11 @@
 import {SelectItemTypeEnum} from '@app/common/components/select-item/select-item-type.enum';
 
-export interface FormPanelSelectConfig {
+export interface    FormPanelSelectConfig {
     required?: boolean;
     barcodeScanner?: boolean;
     searchType?: SelectItemTypeEnum;
     requestParams?: Array<string>;
+    label?: string;
     elements?: Array<{id: number; label: string;}>;
     isMultiple?: boolean;
     filterItem?: (item: any) => boolean;
