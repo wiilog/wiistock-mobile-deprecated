9ce9e8115878a3f3e01d4d0ff2b67cc6c89b4e05
Marwane <marwane.benoukaiss@wiilog.fr>
2022-05-03 12:36:36 +0200

    WIIS-6469: Fix entities

--- src/entities/transport-round-line.ts
+++ src/entities/transport-round-line.ts
@@ -1,9 +1,24 @@
 export interface TransportRoundLine {
-    order_id: number,
-    contact_name: string,
-    contact_address: string,
-    request_type: string,
-    priority: number,
-    estimated_at: string,
-    expected_at: string,
+    id: number;
+    number: string;
+    type: number;
+    status: string;
+    kind: `delivery` | `collect`;
+    packs: Array<{number: string, nature: string, temperature_range: string}>;
+    expected_at: string;
+    estimated_time: string;
+    time_slot: string;
+    contact: {
+        file_number: string;
+        address: string;
+        contact: string;
+        person_to_contact: string;
+        observation: string;
+    };
+    comment: string;
+    photos: Array<string>; // urls des photos
+    signature: string; // url de la signature
+    requester: string;
+    free_fields: Array<{id: number, label: string, value: string}>;
+    priority: number;
 }
--- src/entities/transport-round.ts
+++ src/entities/transport-round.ts
@@ -1,8 +1,11 @@
-import {TransportRoundLine} from "@entities/transport-round-line";
+import {TransportRoundLine} from '@entities/transport-round-line';
 
 export interface TransportRound {
     id: number;
     number: string;
     status: string;
+    date: string;
+    estimated_distance: number;
+    estimated_time: string;
     lines: Array<TransportRoundLine>
 }
